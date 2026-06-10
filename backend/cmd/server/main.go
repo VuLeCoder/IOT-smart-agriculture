54eb0bd78524fff7e255b52ddf19c7e5a386fa2c
VuzLe <tle63917@gmail.com>
2026-06-10 15:26:07 +0700

    feat(backend): Khởi tạo main server và kết nối database

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"IOT-Smart-Agriculture/internal/config"
+	"IOT-Smart-Agriculture/internal/database"
+	"IOT-Smart-Agriculture/internal/router"
+	"IOT-Smart-Agriculture/utils/dependency"
+	"log"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Configuration loaded")
+
+	db, err := database.Connect(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Database connected")
+
+	defer db.Close()
+
+	depInject := dependency.CreateNewDI(db)
+
+	r := router.Setup(depInject)
+
+	log.Printf("server started at :%s", cfg.Port)
+
+	if err := r.Run(":" + cfg.Port); err != nil {
+		log.Fatal(err)
+	}
+}
