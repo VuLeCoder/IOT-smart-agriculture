57f74a5ab6157424fcfbcece88d04bb138c398b3
VuzLe <tle63917@gmail.com>
2026-06-10 15:25:32 +0700

    feat(backend): Đăng ký các API liên quan đến việc gửi, nhận dữ liệu từ sensorIOT

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,34 +1,26 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5/pgxpool"
 
-	"IOT-Smart-Agriculture/internal/config"
+	"IOT-Smart-Agriculture/internal/middlewares"
+	"IOT-Smart-Agriculture/utils/dependency"
 )
 
-type Router struct {
-	Config *config.Config
-	DB     *pgxpool.Pool
-}
-
-func Setup(cfg *config.Config, db *pgxpool.Pool) *gin.Engine {
+func Setup(di *dependency.DI) *gin.Engine {
 	r := gin.Default()
+	api := r.Group("/IOT-api")
 
-	r.GET("/health", func(c *gin.Context) {
-		if err := db.Ping(c.Request.Context()); err != nil {
-			c.JSON(http.StatusServiceUnavailable, gin.H{
-				"status": "database unavailable",
-			})
-			return
-		}
+	device := api.Group("/device")
+	device.Use(middlewares.DeviceAuthMiddleware(di.DeviceRepo))
+	{
+		device.POST("/sensor-data", di.SensorHandler.SaveData)
+	}
 
-		c.JSON(http.StatusOK, gin.H{
-			"status": "ok",
-		})
-	})
+	web := api.Group("/web")
+	{
+		web.GET("/devices/:deviceID/sensor-data", di.SensorHandler.GetData)
+	}
 
 	return r
 }
